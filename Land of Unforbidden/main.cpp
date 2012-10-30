6f0effa90549827c226f44b4b221741275bda580
Tom Gorup <trynreadme@live.com>
2012-10-29 22:15:29 -0500

    Comments removed
    Removed a few comments

--- Land of Unforbidden/main.cpp
+++ Land of Unforbidden/main.cpp
@@ -1,10 +1,6 @@
 //
 //  main.cpp
 //  Land of Unforbidden
-//
-//  Created by Thomas Gorup on 10/29/12.
-//  Copyright (c) 2012 Thomas Gorup. All rights reserved.
-//
 
 #include <iostream>
 #include <cmath>

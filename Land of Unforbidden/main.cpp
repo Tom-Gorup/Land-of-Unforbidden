1fdae86924111169af42c087738ab1ccbd97dc3e
Tom Gorup <trynreadme@live.com>
2012-10-29 20:59:44 -0500

    Initial Commit

---
+++ Land of Unforbidden/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Land of Unforbidden
+//
+//  Created by Thomas Gorup on 10/29/12.
+//  Copyright (c) 2012 Thomas Gorup. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
